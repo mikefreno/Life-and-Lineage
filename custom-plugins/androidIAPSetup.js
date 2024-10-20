6d090f5ba0479920ff4cce110e3ee1ef00e375e7
Michael Freno <michaelt.freno@gmail.com>
2024-10-19 22:12:51 -0400

    adding more to automation

--- custom-plugins/androidIAPSetup.js
+++ custom-plugins/androidIAPSetup.js
@@ -2,7 +2,7 @@ const { withDangerousMod } = require("@expo/config-plugins");
 const fs = require("fs");
 const path = require("path");
 
-function modifyBuildGradle(contents) {
+function modifyRootBuildGradle(contents) {
   // Check if supportLibVersion is already defined
   if (!contents.includes("supportLibVersion")) {
     // Find the ext block and add supportLibVersion
@@ -15,17 +15,49 @@ function modifyBuildGradle(contents) {
   return contents;
 }
 
+function modifyAppBuildGradle(contents) {
+  // Check if missingDimensionStrategy is already defined
+  if (!contents.includes('missingDimensionStrategy "store", "play"')) {
+    // Find the defaultConfig block and add missingDimensionStrategy
+    return contents.replace(/defaultConfig\s*{[^}]*}/, (match) => {
+      return (
+        match.slice(0, -1) +
+        '    missingDimensionStrategy "store", "play"\n    }'
+      );
+    });
+  }
+  return contents;
+}
+
 module.exports = function withCustomAndroidConfig(config) {
   return withDangerousMod(config, [
     "android",
     async (config) => {
-      const buildGradlePath = path.join(
+      // Modify root build.gradle
+      const rootBuildGradlePath = path.join(
         config.modRequest.platformProjectRoot,
         "build.gradle",
       );
-      const contents = await fs.promises.readFile(buildGradlePath, "utf8");
-      const modifiedContents = modifyBuildGradle(contents);
-      await fs.promises.writeFile(buildGradlePath, modifiedContents);
+      const rootContents = await fs.promises.readFile(
+        rootBuildGradlePath,
+        "utf8",
+      );
+      const modifiedRootContents = modifyRootBuildGradle(rootContents);
+      await fs.promises.writeFile(rootBuildGradlePath, modifiedRootContents);
+
+      // Modify app/build.gradle
+      const appBuildGradlePath = path.join(
+        config.modRequest.platformProjectRoot,
+        "app",
+        "build.gradle",
+      );
+      const appContents = await fs.promises.readFile(
+        appBuildGradlePath,
+        "utf8",
+      );
+      const modifiedAppContents = modifyAppBuildGradle(appContents);
+      await fs.promises.writeFile(appBuildGradlePath, modifiedAppContents);
+
       return config;
     },
   ]);
