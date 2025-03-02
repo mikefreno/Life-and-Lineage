8a8d423346eb0951d5d0b73339ec240261ace815
Michael Freno <michaelt.freno@gmail.com>
2025-03-02 15:19:06 -0500

    beginning work on IAPs

---
+++ stores/IAPStore.ts
@@ -0,0 +1,37 @@
+import { action, makeObservable, observable } from "mobx";
+import { RootStore } from "./RootStore";
+import { Platform } from "react-native";
+
+export class IAPStore {
+  root: RootStore;
+  remoteBackups = false;
+  rangerUnlocked = false;
+  necromancerUnlocked = false;
+
+  constructor({ root }: { root: RootStore }) {
+    this.root = root;
+
+    makeObservable(this, {
+      remoteBackups: observable,
+      rangerUnlocked: observable,
+      necromancerUnlocked: observable,
+      unlockRemoteBackups: action,
+    });
+  }
+
+  syncWithAppStore() {
+    if (this.root.authStore.isConnected) {
+      if (Platform.OS == "ios" || Platform.OS == "macos") {
+        //talk to app store
+      } else if (Platform.OS == "android") {
+        //talk to google play
+      } else {
+        //talk to stripe
+      }
+    }
+  }
+
+  unlockRemoteBackups() {
+    this.remoteBackups = true;
+  }
+}
